4759fa75f317825853d3e0ffc66a3ca12c5c34d8
vipulyadav150 <vipulyadav150@gmail.com>
2018-03-12 22:27:06 +0530

    Major change in crawler code.Picks up Brochure download links if present.

--- Python_Web_crawler.py
+++ Python_Web_crawler.py
@@ -12,9 +12,40 @@ def spider_build():
 
         for link in soup.findAll('a',{'rel':'nofollow'}):
             title = link.string
-       
+
             href = link.get('href')
             print('Title : ' + title)
             print(href)
+            brochure_download_link(href)
+
+
+def brochure_download_link(main_url):
+    source_code = requests.get(main_url)
+    plain_text = source_code.text
+    soup = BeautifulSoup(plain_text,"html.parser")
+    #print(soup)
+    for link_down in soup.findAll('div',{'class':'customer-services'}):
+      #  for head in link_down.findAll('h2'):
+          #  print(head)
+            #for un_list in head.findAll('ul'):
+             for un_list in link_down.findAll('ul'):
+              #  print(un_list.findAll('li')[0].findAll('a')[0])
+                l = un_list.findAll('li')[0].findAll('a')[0]
+                # for list in un_list.findAll('li'):
+                #  for link_fresh in list.findAll('a'):
+                href_new = str(main_url) + l.get('href')
+                print("Download Brochure from : " + href_new)
+
+
+
+        #title_new = link_down.string
+        #print(title_new)
+
+
+
+
+
+
+
 
 spider_build()
